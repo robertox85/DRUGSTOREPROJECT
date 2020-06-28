56d58714b9e228fea2ec1b17ad8bab4309d64c81
robertox85 <dimarco.roberto85@gmail.com>
2020-06-28 19:56:21 +0200

    Push code

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,6 +42,52 @@ function pageTransition() {
   });
 }
 
+document.addEventListener('click', function (event) {
+
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.servizio-link')) return;
+
+	// Don't follow the link
+  event.preventDefault();
+  
+
+  
+  contentAnimation();
+
+  setTimeout(function(){
+    renderHello(event.target.dataset);  
+  },1000)
+
+}, false);
+
+
+function renderHello(data) {
+  // var template = document.getElementById('template').innerHTML;
+  var titolo = document.getElementById('template-titolo').innerHTML;
+  var contenuto = document.getElementById('template-contenuto').innerHTML;
+  var lista = document.getElementById('template-lista').innerHTML;
+  var counter = document.getElementById('template-counter').innerHTML;
+
+  var json = { 
+    titolo: data.titolo,
+    contenuto: data.contenuto,
+    items: data.items.split(';')
+  };
+  
+  var renderedTitolo = Mustache.render(titolo, {titolo:data.titolo});
+  var renderedContenuto = Mustache.render(contenuto, {contenuto:data.contenuto});
+  var renderedLista = Mustache.render(lista, {items:data.items.split(';')});
+  var renderedCounter = Mustache.render(counter, {counter:data.counter});
+
+  document.getElementById('data-titolo').innerHTML = renderedTitolo;
+  document.getElementById('data-contenuto').innerHTML = renderedContenuto;
+  document.getElementById('data-lista').innerHTML = renderedLista;
+  document.getElementById('data-counter').innerHTML = renderedCounter;
+
+  
+  
+}
+
 function contentAnimation() {
   var tl = gsap.timeline();
   var tl2 = gsap.timeline();
@@ -49,16 +95,39 @@ function contentAnimation() {
   tl.from(".guido img, .guido h1,.guido h2,.guido h3,.guido h4,.guido p", {  duration: 1.2, translateX: 50, opacity: 0, stagger: 0.2 });
   tl2.from(".mirko img, .mirko h1,.mirko h2,.mirko h3,.mirko h4,.mirko p", { duration: 1.2, translateX: 50, opacity: 0, stagger: 0.2 });
   // tl3.from(".azienda h1,.azienda h2,.azienda h3,.azienda h4,.azienda p", {   duration: 1.2, translateX: 50, opacity: 0, stagger: 0.2 });
+
+
+  
+  var tl4 = gsap.timeline();
+  var tl5 = gsap.timeline();
+  tl5
+  .to('.number', {          duration: .8,  opacity:0,   scale:100,  ease:  Power2.easeInOut})
+  .to('.number', {delay:.4, duration: 0.3, opacity:.4,  scale:1,    ease:  Power2.easeInOut}, "-=0.1")
+
+  tl4
+  .to('.text__first-bg',     .5, {scaleX:1,   ease:  Power2.easeInOut, stagger:0.1})
+  .to('.text__word',             {opacity:1,  ease:  Power2.easeInOut}, "-=0.1")
+  .to('.text__first-bg',         {scaleX:0,   ease:  Power2.easeInOut})
+  
+  
+  
   
 }
 
 function leaveAnimation() {
-  var tl = gsap.timeline();
-  var tl2 = gsap.timeline();
-  var tl3 = gsap.timeline();
-  tl.from(".guido img, .guido h1,.guido h2,.guido h3,.guido h4,.guido p", {  duration: 1.2, translateX: -50, opacity: 0, stagger: 0.1 });
-  tl2.from(".mirko img, .mirko h1,.mirko h2,.mirko h3,.mirko h4,.mirko p", { duration: 1.2, translateX: -50, opacity: 0, stagger: 0.1 });
+  // var tl = gsap.timeline();
+  // var tl2 = gsap.timeline();
+  // var tl3 = gsap.timeline();
+  // tl.from(".guido img, .guido h1,.guido h2,.guido h3,.guido h4,.guido p", {  duration: 1.2, translateX: -50, opacity: 0, stagger: 0.1 });
+  // tl2.from(".mirko img, .mirko h1,.mirko h2,.mirko h3,.mirko h4,.mirko p", { duration: 1.2, translateX: -50, opacity: 0, stagger: 0.1 });
   // tl3.from(".azienda h1,.azienda h2,.azienda h3,.azienda h4,.azienda p", {   duration: 1.2, translateX: -50, opacity: 0, stagger: 0.1 });
+
+  var tl4 = gsap.timeline();
+  tl4
+  .to('.text__first-bg', {scaleX:1,   ease:  Power2.easeInOut})
+  .to('.text__word',     {opacity:1,  ease:  Power2.easeInOut}, "-=0.1")
+  .to('.text__first-bg', {scaleX:0,   ease:  Power2.easeInOut})
+  
   
 }
 
